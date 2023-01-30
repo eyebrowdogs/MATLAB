03deb61b0f72254ad1a04c4c0cfc7a904b4b6e40
eyebrowdogs <99934797+eyebrowdogs@users.noreply.github.com>
2023-01-29 20:47:57 -0600

    matlab git broken, commiting lates changes
    tracked verboser

---
+++ verboser.m
@@ -0,0 +1,13 @@
+function verboser(v,groots)
+ disp(" ")
+        disp(" ")
+        disp("Resultados")
+        disp(" ")
+        disp("Raiz: ")    %resultado final si p =0
+        fprintf('%.8f', groots(v))
+        disp(" ")
+        disp(" ")
+        disp("Iteraciones:")
+        disp(v)
+
+end
